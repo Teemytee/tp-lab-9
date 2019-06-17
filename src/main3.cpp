89aa6d4c17134fb46b9a1fb0c8e263299bc77e32
Pavel Semkin <pvsemk@mail.ru>
2019-06-17 19:45:44 +0300

    Done task3

--- src/main3.cpp
+++ src/main3.cpp
@@ -1,4 +1,59 @@
-//
-// Created by pavel on 17.06.19.
-//
+#include <thread>
+#include <iostream>
+#include <queue>
+#include <mutex>
+#include <vector>
+#include <ctime>
+#include <random>
+
+using namespace std;
+mutex mu;
+
+void serve(queue<vector<int>> cust)
+{
+    lock_guard<mutex> lockGuard(mu);
+    cout << "Cashbox: " << this_thread::get_id() << " with " << cust.size() << " customers"
+    << endl;
+    for (int i(1); !cust.empty(); i++)
+    {
+        cout << "Checking " << i << " with " << cust.front().size() << " goods" << endl;
+        for (int j(0); j < cust.front().size(); j++)
+        {
+            this_thread::sleep_for(chrono::milliseconds(200));
+        }
+        cust.pop();
+    }
+}
+
+int main()
+{
+    mt19937 gen(time(NULL));
+
+    queue<vector<int>> customers;
+    vector<thread*> threads;
+    vector <int> goods;
+    int customerNumber = gen() % 16, goodsNumber;
+    cout << "We have " << customerNumber << " overall" << endl;
+
+    for (int i(0); i < customerNumber; i++)
+    {
+        goodsNumber = 1 + gen() % 10;
+        while (goodsNumber--)
+        {
+            int price = 1 + gen() % 500;
+            goods.push_back(price);
+        }
+        customers.push(goods);
+        goods.clear();
+        if ((customers.size() % 5 == 0) || (i == customerNumber - 1))
+        {
+            threads.push_back(new thread(serve, customers));
+            while(!customers.empty())
+                customers.pop();
+        }
+    }
+    for (auto& th : threads)
+        th->join();
+
+}
 
